31c811c761167b2a8b125f670548f66a7290a605
XxJamikenxX <jamiubom@gmail.com>
2026-04-10 17:43:53 -0400

    Update MarkSaver.cs

--- Quest_For_The_Iron_Ring/Assets/Scripts/MarkSaver.cs
+++ Quest_For_The_Iron_Ring/Assets/Scripts/MarkSaver.cs
@@ -136,18 +136,14 @@ public class MarkSaver : MonoBehaviour
 
     public bool CanEnterLevelInOrder(string levelName)
     {
-        // Find index of this level
         int index = System.Array.IndexOf(levelNames, levelName);
 
-        // If not found, block it
         if (index == -1)
             return false;
 
-        // Level1 is always accessible
         if (index == 0)
             return true;
 
-        // Check ALL previous levels
         for (int i = 0; i < index; i++)
         {
             if (!HasPassedLevel(levelNames[i]))
