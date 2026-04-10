8d7b5e74f8bd059a37cac7e9d280d2e7315b523d
XxJamikenxX <jamiubom@gmail.com>
2026-04-10 15:47:40 -0400

    Revert "Merge branch 'main' into grade-feature"
    This reverts commit 9c24cba5520df32f5e93bbb329671beec63aba74, reversing
    changes made to cad59408a5a2c63b316d26955ae497cb9ce9993e.

--- Quest_For_The_Iron_Ring/Assets/Scripts/playerMovement.cs
+++ Quest_For_The_Iron_Ring/Assets/Scripts/playerMovement.cs
@@ -52,12 +52,6 @@ public class playerMovement : MonoBehaviour
         }
     }
 
-    // for level 3
-    public void SetMoveSpeed(float newSpeed)
-    {
-        moveSpeed = newSpeed;
-    }
-
     public void SetNearbyBug(Bug bug)
     {
         nearbyBug = bug;
