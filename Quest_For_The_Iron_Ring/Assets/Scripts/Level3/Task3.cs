c6f2a6e71b1b87b5bb4bda3106db453be0b26bcc
JacobRadcliffe <jradcli@uwo.ca>
2026-04-10 17:22:08 -0400

    Reapply "Testing"
    This reverts commit 5f7148ac7d685a2b9e4c517ffb73618020424822.

--- Quest_For_The_Iron_Ring/Assets/Scripts/Level3/Task3.cs
+++ Quest_For_The_Iron_Ring/Assets/Scripts/Level3/Task3.cs
@@ -8,6 +8,7 @@ public class Task3 : MonoBehaviour
     [SerializeField] private float levelTime = 180f;
     [SerializeField] private string hubSceneName = "Hallway";
     [SerializeField] private int maxIconsForGrade = 20;
+    [SerializeField] public float FinalGrade = 0; // final grade for jami
     [SerializeField] private float endScreenDuration = 10f;
 
     [Header("Phase Times")]
@@ -152,6 +153,7 @@ public class Task3 : MonoBehaviour
 
         levelEnded = true;
         ShowWinScreen();
+        FinalGrade = ((float)iconsCollected / maxIconsForGrade) * 100f; // final grade of jami
 
         Invoke(nameof(ReturnToHub), endScreenDuration);
     }
